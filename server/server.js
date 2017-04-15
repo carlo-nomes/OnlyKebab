47528c72de544bd6818ac35f68375f00c5053210
Carlo Nomes <carlo.nomes@student.kdg.be>
2017-04-15 18:05:25 +0200

    Added 3th party auth (Facebook)

--- server/server.js
+++ server/server.js
@@ -2,12 +2,39 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
 var app = module.exports = loopback();
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
-app.start = function () {
+
+// Passport configurators..
+var loopbackPassport = require('loopback-component-passport');
+var PassportConfigurator = loopbackPassport.PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+// attempt to build the providers/passport config
+var config = {};
+try {
+  config = require('./providers.json');
+} catch (err) {
+  console.error('Failed to load providers.json');
+  console.trace(err);
+  process.exit(1); // fatal
+}
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
+
+app.start = function() {
   // start the web server
-  return app.listen(function () {
+  return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
@@ -18,12 +45,27 @@ app.start = function () {
   });
 };
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function (err) {
-  if (err) throw err;
+// The access token is only available after boot
+app.middleware('auth', loopback.token({
+  model: app.models.accessToken
+}));
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+app.middleware('session:before', cookieParser(app.get('cookieSecret')));
+app.middleware('session', session({
+  secret: 'Kitty',
+  saveUninitialized: true,
+  resave: true
+}));
+passportConfigurator.init();
+
+passportConfigurator.setupModels({
+  userModel: app.models.user,
+  userIdentityModel: app.models.userIdentity,
+  userCredentialModel: app.models.userCredential
 });
+// Configure passport strategies for third party auth providers
+for (var s in config) {
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
+}
