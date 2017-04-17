e20fcaf39b712114200b91c73ac46be4bdc90e94
Carlo Nomes <carlo.nomes@student.kdg.be>
2017-04-17 17:55:07 +0200

    Added remote hook for create consumption

--- common/models/consumption.js
+++ common/models/consumption.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = function(Consumption) {
-
+  Consumption.beforeRemote('create', function(context, user, next) {
+    context.args.data.timestamp = Date.now();
+    context.args.data.consumerId = context.req.accessToken.userId;
+    next();
+  })
 };
