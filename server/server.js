6543928bd2cd89084dc8f4cad3f5307f390bb57b
Carlo Nomes <carlo.nomes@student.kdg.be>
2017-08-06 17:13:02 +0200

    Added 3th party auth (Facebook)

--- server/server.js
+++ server/server.js
@@ -12,6 +12,16 @@ var loopbackPassport = require('loopback-component-passport');
 var PassportConfigurator = loopbackPassport.PassportConfigurator;
 var passportConfigurator = new PassportConfigurator(app);
 
+/**
+ * Flash messages for passport
+ *
+ * Setting the failureFlash option to true instructs Passport to flash an
+ * error message using the message given by the strategy's verify callback,
+ * if any. This is often the best approach, because the verify callback
+ * can make the most accurate determination of why authentication failed.
+ */
+var flash = require('express-flash');
+
 // attempt to build the providers/passport config
 var config = {};
 try {
@@ -24,13 +34,35 @@ try {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
+boot(app, __dirname);
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+// The access token is only available after boot
+app.middleware('auth', loopback.token({
+  model: app.models.accessToken
+}));
+
+app.middleware('session:before', cookieParser(app.get('cookieSecret')));
+app.middleware('session', session({
+  secret: 'OK Is Best Kebab',
+  saveUninitialized: true,
+  resave: true
+}));
+passportConfigurator.init();
+
+// We need flash messages to see passport errors
+app.use(flash());
+
+passportConfigurator.setupModels({
+  userModel: app.models.user,
+  userIdentityModel: app.models.userIdentity,
+  userCredentialModel: app.models.userCredential
 });
+// Configure passport strategies for third party auth providers
+for (var s in config) {
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
+}
 
 app.start = function() {
   // start the web server
@@ -45,27 +77,6 @@ app.start = function() {
   });
 };
 
-// The access token is only available after boot
-app.middleware('auth', loopback.token({
-  model: app.models.accessToken
-}));
-
-app.middleware('session:before', cookieParser(app.get('cookieSecret')));
-app.middleware('session', session({
-  secret: 'Kitty',
-  saveUninitialized: true,
-  resave: true
-}));
-passportConfigurator.init();
-
-passportConfigurator.setupModels({
-  userModel: app.models.user,
-  userIdentityModel: app.models.userIdentity,
-  userCredentialModel: app.models.userCredential
-});
-// Configure passport strategies for third party auth providers
-for (var s in config) {
-  var c = config[s];
-  c.session = c.session !== false;
-  passportConfigurator.configureProvider(s, c);
-}
+// start the server if `$ node server.js`
+if (require.main === module)
+  app.start();
